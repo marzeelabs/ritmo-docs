712e434b4ff59615abd4723ef9e0c745408554b9
pedrosantos <pedro.d.santos.design@gmail.com>
2016-08-04 18:10:52 +0100

    Ritmo #4 - more code cleanup and styling done

---
+++ public/_scripts/site.js
@@ -0,0 +1,67 @@
+$(function() {
+
+  var breakMobile = 730, // viewport px breakpoint
+
+      waitForScroll = false,
+
+      triggerHashChange = function() {
+        $( window ).trigger('hashchange');
+      },
+
+  scrollToSection = function() {
+
+    var $sectionElement = $("section" + location.hash + '-section');
+
+    if ($sectionElement.length) {
+
+      waitForScroll = true;
+
+      // We can't use $(".navigation").outerHeight() because
+      // we'd need to wait for the CSS animation to finish.
+      $("html, body").stop().animate({
+        scrollTop: $sectionElement.scrollTop() + $sectionElement.offset().top ,
+      }, 500, function() {
+        waitForScroll = false;
+      });
+    }
+  },
+
+  fixedHeader = function() {
+    var viewportWidth = $( window ).width(),
+        fixedClass = 'navigation--fixed-top',
+        $navElement = $(".navigation");
+
+    if ($(window).scrollTop() > '1' && viewportWidth >= breakMobile) {
+      $navElement.addClass(fixedClass);
+    } else {
+      $navElement.removeClass(fixedClass);
+    }
+  };
+
+  // Toggle mobile navigation
+  $(".navigation__mobile-menu__toggle").click(function() {
+    $(this).parent().toggleClass('is-open');
+  });
+
+ // Force close mobile navigation when clicking anywhere (except the toggle button itself)
+  $( document ).on('mousedown touchstart', function(event) {
+
+    var selectorElement = '.navigation li a',
+        targetElement = '.navigation__menu__item';
+
+    $(selectorElement).on('click touch', function() {
+      $(".navigation.is-open").removeClass('is-open');
+    });
+
+    // Force close mobile navigation when clicking anywhere (except the toggle button itself)
+    if (!$(event.target).closest(".navigation").length) {
+      $(".navigation.is-open").removeClass('is-open');
+    }
+  });
+
+
+  $(window).on('resize scroll', fixedHeader);
+  $( window ).on('hashchange', scrollToSection).trigger('hashchange');
+  $("a[href^='/#']").click(triggerHashChange);
+
+});
